544c3d6122b874c88d7857f7f603231ccddc5377
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2026-09-07 11:27:32 +0530

    Remove unused backend scripts directory and update package.json

--- backend/scripts/playgound.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-// const Constants = require('@/config/constant');
-
-const files = fs.readdirSync(path.join(__dirname, './../tmp'));
-
-console.log(files);
